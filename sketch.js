7f4e5ca7fce546022ddd065bdd62cb8469ec9eb8
Krishnanischal <krishnanischal06@gmail.com>
2020-08-10 18:54:50 +0530

    Database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,50 @@
-var ball;
+var Hball;
+var database;
+var position;
 
 function setup(){
+    database = firebase.database()
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    Hball = createSprite(250,250,10,10);
+    Hball.shapeColor = "red";
+    var Hballposition = database.ref('ball/position');
+    Hballposition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
+    if(position!==undefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
+}
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    Hball.x = ball.x + x;
+    Hball.y = ball.y + y;
+}
+function readPosition(data){
+position=data.val();
+Hball.x=position.x;
+Hball.y=position.y;
+}
+function showError(){
+    console.log("Error");
+}
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
 }
